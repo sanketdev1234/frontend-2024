f07f0cf901abadd37b601ea2775ee89370f26c66
sanketdev1234 <codetech28@gmail.com>
2025-04-04 21:29:06 +0530

    initial commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,19 +1,12 @@
 import React from 'react'
 import iiit_logo from '../images/iiit_surat.png'
 import spring from '../images/Spring.png'
-import {Link} from 'react-router-dom'
-import Horizontal from '../components/horrizontal-pipe.svg'
-
 const Footer = () => {
   return (
-    <div >
-      <img className='relative mid-line w-full' src={Horizontal} alt="horizontal" />
-        <div className='overflow-x-hidden mt-[10px] md:mt-[-45px] bg-black'>
-
-        
-         <div className='flex justify-between items-center h-full mt-10 py-4 md:py-12 md:px-6'>
+    <div className=' bg-black'>
+         <div className='flex padding-bottom:10vw justify-start md:justify-between items-center h-full py-4 md:py-5 md:px-8 '>
             <div className='flex flex-col'>
-                <div className='md:flex md:gap-4 ml-6 mt-2 md:mt-16  md:ml-20'>
+                <div className='md:flex md:gap-4 mt-2 md:mt-16  md:ml-20'>
                     <div className='w-20 md:w-28 scale-75 md:scale-100'>
                         <img src={iiit_logo} alt="IIIT Surat Logo" className='w-full h-auto' />
                     </div>
@@ -26,7 +19,7 @@ const Footer = () => {
                 </div>
             </div>
 
-            <div className='flex justify-between md:justify-center items-center md:gap-14 text-white mt-2 lg:mr-20 md:mt-0 scale-75 md:scale-100 '>
+            <div className='flex justify-start md:justify-center items-center md:gap-14 text-white mt-2 md:mt-0 scale-75 md:scale-100 '>
                     <div className='text-xs md:text-base scale-90 md:scale-100'>
                     <div className='text-base md:text-xl font-normal'>Contact</div>
                     <div className='mt-1 md:mt-3 text-xs md:text-base font-normal'>Indian Institute of information</div>
@@ -38,23 +31,11 @@ const Footer = () => {
                 </div>
 
                 <div className='flex flex-col gap-2 md:gap-8 text-xs md:text-base max-sm:scale-110'>
-                   <Link to="/">
                     <div className='font-normal'>Home</div>
-                    
-                   </Link>
-                   <Link to="/events">
                     <div className='font-normal'>Events</div>
-                    
-                   </Link>
-                   <Link to="/core-team">
                     <div className='font-normal'>Core Team</div>
-                   </Link>
-                   <Link to="/sponsors">
-                    <div className='font-normal'>Sponsors</div>
-                   </Link>
-                   <Link to="/gallery">
                     <div className='font-normal'>Gallery</div>
-                   </Link>
+                    <div className='font-normal'>Sponsors</div>
 
                     
                 </div> 
@@ -68,8 +49,6 @@ const Footer = () => {
                 &copy; 2025 spring fiesta
                 </div>
     </div>
-            </div>
-
   )
 }
 
